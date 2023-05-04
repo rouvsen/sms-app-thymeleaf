9ee7de8725cd1e56bf08602428533dba350ce0ce
rovshan <rouvsen@gmail.com>
2023-05-04 16:17:19 +0400

    Student Service Imp. added

---
+++ src/main/java/com/rouvsen/studentmanagementsystem/service/impl/StudentServiceImpl.java
@@ -0,0 +1,44 @@
+package com.rouvsen.studentmanagementsystem.service.impl;
+
+import com.rouvsen.studentmanagementsystem.entity.Student;
+import com.rouvsen.studentmanagementsystem.repository.StudentRepository;
+import com.rouvsen.studentmanagementsystem.service.StudentService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public Student getStudentById(Long id) {
+        return studentRepository.findById(id).get();
+    }
+
+    @Override
+    public Student updateStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public void deleteStudentById(Long id) {
+        studentRepository.deleteById(id);
+    }
+
+}
