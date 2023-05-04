993488c4dc5d0875829b4d480a69d6a2f614b014
rovshan <rouvsen@gmail.com>
2023-05-04 16:17:13 +0400

    Student Service added

---
+++ src/main/java/com/rouvsen/studentmanagementsystem/service/StudentService.java
@@ -0,0 +1,17 @@
+package com.rouvsen.studentmanagementsystem.service;
+
+import com.rouvsen.studentmanagementsystem.entity.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    List<Student> getAllStudents();
+
+    Student saveStudent(Student student);
+
+    Student getStudentById(Long id);
+
+    Student updateStudent(Student student);
+
+    void deleteStudentById(Long id);
+}
